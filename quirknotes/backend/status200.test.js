62b30862e8a279b87b2510464d495dd212cb89d3
Kenny Wu <kennywu@KennydeMacBook-Pro.local>
2024-02-15 16:49:52 -0500

    updated test

--- quirknotes/backend/status200.test.js
+++ quirknotes/backend/status200.test.js
@@ -1,3 +1,42 @@
+import express from "express";
+import { MongoClient, ObjectId } from "mongodb";
+import cors from "cors";
+
+const app = express();
+const PORT = 4000;
+const mongoURL = "mongodb://127.0.0.1:27017";
+const dbName = "quirknotes";
+
+// Connect to MongoDB
+let db;
+
+async function connectToMongo() {
+  const client = new MongoClient(mongoURL);
+
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    db = client.db(dbName);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+connectToMongo();
+
+// Open Port
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+
+app.use(cors());
+
+// Collections to manage
+const COLLECTIONS = {
+    notes: "notes",
+  };
+
 test("1+2=3, empty array is empty", () => {
     expect(1 + 2).toBe(3);
     expect([].length).toBe(0);
